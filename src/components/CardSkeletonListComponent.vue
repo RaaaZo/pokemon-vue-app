6d6533a701d35cd01e319c36f65910ee51e41949
Mateusz Koprowicz <koprowicz.mateusz@gmail.com>
2023-10-13 14:01:36 +0200

    feat: add skeletons for cards

---
+++ src/components/CardSkeletonListComponent.vue
@@ -0,0 +1,44 @@
+<template>
+  <li v-for="(_, index) in skeletonList" :key="index" :class="$style.skeleton">
+    <div :class="$style[`skeleton-image`]"></div>
+    <div :class="$style[`skeleton-text`]"></div>
+    <div :class="$style[`skeleton-text`]"></div>
+    <div :class="$style[`skeleton-text`]"></div>
+  </li>
+</template>
+
+<script setup lang="ts">
+const amountOfSkeletons = 20
+
+const skeletonList = Array.from({ length: amountOfSkeletons })
+</script>
+
+<style module>
+.skeleton {
+  animation: skeleton-loading 1s linear infinite alternate;
+  width: 100%;
+  height: auto;
+  border-radius: 0.25rem;
+}
+
+@keyframes skeleton-loading {
+  0% {
+    background-color: var(--gray-light);
+  }
+  100% {
+    background-color: var(--gray);
+  }
+}
+
+.skeleton-image {
+  width: 100%;
+  height: 29.4rem;
+}
+
+.skeleton-text {
+  width: 100%;
+  height: 0.7rem;
+  margin-bottom: 0.5rem;
+  border-radius: 0.25rem;
+}
+</style>
--- src/components/PokemonCardsList.vue
+++ src/components/PokemonCardsList.vue
@@ -5,12 +5,15 @@
         <PokemonCard :pokemon="pokemon" />
       </li>
     </ul>
-    <p v-if="error">{{ error }}</p>
-    <p v-if="loading">Loading</p>
+
+    <ul :class="$style[`list-wrapper`]">
+      <CardSkeletonList v-if="loading" />
+    </ul>
 
     <PaginationComponentVue
-      :is-next-button-disabled="!nextPage"
-      :is-prev-button-disabled="!previousPage"
+      v-if="!loading"
+      :is-next-button-disabled="isNextButtonDisabled"
+      :is-prev-button-disabled="isPrevButtonDisabled"
       @on-next-button-click-handler="() => onUrlChange(nextPage)"
       @on-prev-button-click-handler="() => onUrlChange(previousPage)"
     />
@@ -18,6 +21,7 @@
 </template>
 
 <script setup lang="ts">
+import CardSkeletonList from '@/components/CardSkeletonListComponent.vue'
 import PaginationComponentVue from '@/components/PaginationComponent.vue'
 import PokemonCard from '@/components/PokemonCard.vue'
 import { useFetchPokemonsList } from '@/composables/fetchPokemonsList'
@@ -26,6 +30,9 @@ import { ref } from 'vue'
 const url = ref(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=0`)
 
 const { pokemonList, error, loading, nextPage, previousPage } = useFetchPokemonsList(url)
+
+const isNextButtonDisabled = !nextPage || !!error
+const isPrevButtonDisabled = !previousPage || !!error
 
 const onUrlChange = (newUrl: string | null) => {
   if (!newUrl) return
